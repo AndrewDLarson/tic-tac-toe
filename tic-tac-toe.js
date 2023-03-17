bfe71e0074c90b93027f7ddaed3b14c3573425df
Andrew Larson <Andrewlarson1989@gmail.com>
2023-03-17 17:42:13 -0500

    make function to enable play

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,40 @@
-df
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const outcome = document.getElementById("outcome");
+
+const addO = (spot) => {
+    spot.innerHTML = "O";
+};
+
+const addX = (spot) => {
+    spot.innerHTML ="X"
+};
+
+one.onclick = function() {
+    one.innerHTML = "X";
+    addO(nine);
+    three.onclick = function() {
+        addX(three);
+        addO(two);
+        four.onclick = function() {
+            addX(four);
+            addO(five);
+            six.onclick = function() {
+                addX(six);
+                addO(eight);
+                seven.onclick = function() {
+                    addX(seven)
+                    outcome.innerHTML = "loser";
+                }
+            }
+        }
+    }
+}
+
